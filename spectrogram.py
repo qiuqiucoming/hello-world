d0798fb5e4a797241cdf50fa74c9ce839ee9bf81
qiuqiucoming <2579948582@qq.com>
2020-04-19 20:07:13 +0800

    Update spectrogram.py
    解析音频将时域通过傅里叶变换转化为频率（该 转换接口的效果好像比librosa.feature.melspectrogram要好，感觉）

--- spectrogram.py
+++ spectrogram.py
@@ -1,3 +1,10 @@
+import librosa
+from IPython.display import Audio
+from librosa import display
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+#将时域转化为频谱的函数
 def spectrogram(samples, sample_rate, stride_ms = 10.0, 
                           window_ms = 20.0, max_freq = None, eps = 1e-14):
 
@@ -32,3 +39,16 @@ def spectrogram(samples, sample_rate, stride_ms = 10.0,
     ind = np.where(freqs <= max_freq)[0][-1] + 1
     specgram = np.log(fft[:ind, :] + eps)
     return specgram
+#利用librosa解析出音频的时域峰值以及采样频率
+file_path = "D:/Documents/dev-clean/LibriSpeech/dev-clean/84/121123/84-121123-0000.flac"
+#其中samples， sampling_rate都是一个列表
+samples, sampling_rate = librosa.load(file_path, sr = None, mono = True, offset = 0.0, duration = None)
+#由接口spectrogram可以获取音频对应的分贝频谱，其格式为一个多维数组
+specgrams = spectrogram(samples, sampling_rate, max_freq = 8000)
+#获取频谱之后就直接画图了，接口是librosa.display.specshow()
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(specgrams, x_axis='time', y_axis='mel',sr=sampling_rate,fmax=8000)
+plt.colorbar(format='%+2.0f db')
+plt.title("mel specgram")
+plt.tight_layout()
+plt.show()
